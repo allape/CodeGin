168f1b23788016162d5b164d0a0c4d561a905317
as <allensnape@qq.com>
2021-06-10 17:31:37 +0800

    *) 完善react打包

--- react-app/move-to-electron-js.js
+++ react-app/move-to-electron-js.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 const APP_FOLDER = '../electron-js/app/';
-fs.unlinkSync(APP_FOLDER)
+if (fs.existsSync(APP_FOLDER)) fs.rmdirSync(APP_FOLDER, { recursive: true });
 // fs.mkdirSync(APP_FOLDER, { recursive: true });
 fs.renameSync('./build', APP_FOLDER);
