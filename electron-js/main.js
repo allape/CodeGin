a1d99ba65cd61d108c405a2270887f0266725f96
as <allensnape@qq.com>
2021-06-11 10:36:54 +0800

    *) 完善

--- electron-js/main.js
+++ electron-js/main.js
@@ -47,6 +47,7 @@ app.whenReady().then(() => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     });
 
+    // 模板文件
     ipcMain.on('get-saved-tpl-files', e => {
         try {
             if (!fs.existsSync(SAVED_TEMPLATE_FILES_FOLDER)) {
@@ -67,20 +68,41 @@ app.whenReady().then(() => {
             e.returnValue = err;
         }
     });
+
+    // 缓存连接信息
+    let conn = null;
+
     const GetConnectionInfoChannel = 'get-connection-info';
     ipcMain.on(GetConnectionInfoChannel, (e, args) => {
         try {
-            const conn = mysql.createConnection({
+            conn = mysql.createConnection({
                 ...args,
                 user: args.username,
                 timeout: 10000,
             });
             execute(conn, `SHOW DATABASES`).then(res => {
-                console.log(res);
-                e.reply(GetConnectionInfoChannel, []);
+                e.reply(GetConnectionInfoChannel, {
+                    name: '',
+                    schemas: res.results.map(i => ({
+                        name: i.Database,
+                    })),
+                });
             }).catch(err => e.reply(GetConnectionInfoChannel, err));
         } catch (err) {
             e.reply(GetConnectionInfoChannel, err);
+        }
+    });
+
+    const GetTablesChannel = 'get-tables';
+    ipcMain.on(GetTablesChannel, (e, args) => {
+        try {
+            execute(conn, `SHOW TABLES FROM \`${args}\``).then(res => {
+                console.log(res);
+                // TODO
+                e.reply(GetTablesChannel, []);
+            }).catch(err => e.reply(GetTablesChannel, err));
+        } catch (err) {
+            e.reply(GetTablesChannel, err);
         }
     });
 });
--- electron-js/preload.js
+++ electron-js/preload.js
@@ -1,33 +1,5 @@
 const { ipcRenderer } = require('electron');
 
-// async function execute(sql, params) {
-//     return new Promise((resolve, reject) => {
-//         const queried = conn.query(
-//             sql,
-//             params,
-//             (err, results, fields) => {
-//                 if (err) {
-//                     reject(err);
-//                     return;
-//                 }
-//                 resolve({ results, fields });
-//             },
-//         );
-//         console.log(queried.sql);
-//     });
-// }
-//
-// async function gen() {
-//     await execute(
-//         `INSERT INTO person_temp (person_id, meter, rssi, temperature) VALUES (?, ?, ?, ?)`,
-//         [
-//             'b71850b1-6430-11ea-8607-00e269244a8a', '8933',
-//             -20, (Math.random() * 50).toFixed(2),
-//         ],
-//     );
-//     setTimeout(() => gen(), 1001);
-// }
-
 async function ipcRendererWrap(channel, args) {
     return new Promise((resolve, reject) => {
         const result = ipcRenderer.sendSync(channel, args);
@@ -57,6 +29,9 @@ async function ipcRendererAsyncWrap(channel, args) {
 const electron = {
     getConnectionInfo: async (conn) => {
         return ipcRendererAsyncWrap('get-connection-info', conn);
+    },
+    getTables: async (schemaName) => {
+        return ipcRendererAsyncWrap('get-tables', schemaName);
     },
     getSavedTplFiles: async () => {
         return ipcRendererWrap('get-saved-tpl-files');
