06088bd87d2abf4dbfde8c6678fca8b2e465bcdb
as <allensnape@qq.com>
2021-06-16 14:16:07 +0800

    +) 添加对Windows的打包, 未完待续

--- electron-js/pre.js
+++ electron-js/pre.js
@@ -1,3 +1,4 @@
 const fs = require('fs');
 fs.copyFileSync('../resources/icon.png', './icon.png');
 fs.copyFileSync('../resources/icon.icns', './icon.icns');
+fs.copyFileSync('../resources/icon.ico', './icon.ico');
